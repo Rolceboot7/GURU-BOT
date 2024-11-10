1d59225aad4d56e2780c3f2ed178e8afe3e57cb4
Rolceboot7 <141360190+Rolceboot7@users.noreply.github.com>
2024-11-10 18:29:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['242064010125', 'GURU', true],
+  ['242064010125', 'Ash', true], 
   [''] 
 ] //Number of owners
 
